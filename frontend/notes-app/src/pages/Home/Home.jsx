3fe82a1f7c3f0a85aff3f76246a12fbe71e585c3
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-05-20 15:23:00 -0400

    feat: Add lazy loading for images and update Home page layout
    The index.css file was modified to add a new CSS class for icon buttons and improve the styling of the Home page. The Home.jsx file was also updated to include a NoteCard component and a button for adding new notes. These changes enhance the user experience by providing a visually appealing layout and lazy loading for images.

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,13 +1,32 @@
 // Este arquivo é onde a page Home é definido.
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import NoteCard from '../../components/Cards/NoteCard';
+import { MdAdd } from 'react-icons/md';
 // Abaixo é um componente funcional 
 const Home = () => {
     return (
         <>
             <Navbar />
 
-            
+            <div className='container mx-auto'>
+                <div className='grid grid-cols-3 gap-4 mt-8'>
+                    <NoteCard 
+                        title='Note Title'
+                        date='2021-12-12'
+                        content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut mauris ut libero.'
+                        tags="#tag1 #tag2 #tag3"
+                        isPinned={true}
+                        onEdit={() => console.log('Edit Note')}
+                        onDelete={() => console.log('Delete Note')}
+                        onPinNote={() => console.log('Pin Note')}
+                    />
+                </div>
+            </div>
+
+            <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary-200 hover:bg-primary-300 absolute right-10 bottom-10' onClick={() => {}}>
+                <MdAdd className='text-[32px] text-white'/>
+            </button>
         </>
     );
 };
